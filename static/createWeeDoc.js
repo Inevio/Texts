3e2142be0ea7c95c37b5106df271feade9fbfd9c
javiergarmon <jgarmon@weezeel.com>
2014-02-18 18:59:02 +0100

    General [Implementation] Several changes
    Revisión general de posibles bugs

--- static/createWeeDoc.js
+++ static/createWeeDoc.js
@@ -11,21 +11,21 @@
         // Document headers
         result += '<html>';
         result += '<head>';
-        result += '<title>' + ( config.title || '' ) + '</title>'
+        result += '<title>' + ( config.title || '' ) + '</title>';
         result += '<meta name="application-name" content="weeDoc">';
         result += '<meta name="description" content="version:' + moduleVersion + '">';
         result += '<meta name="description" content="creationDate:' + ( ( new Date( config.creationDate ) ).getTime() || Date.now() ) + '">';
         result += '<meta name="description" content="modificationDate:' + ( ( new Date( config.modificationDate ) ).getTime() || Date.now() ) + '">';
         result += '<meta name="author" content="' + ( config.author || '' ) + '">';
         result += '<meta name="generator" content="' + moduleEditor + '">';
-        result += '<meta charset="UTF-8">'
+        result += '<meta charset="UTF-8">';
         result += '</head>';
         result += '<body>';
 
         // Content
         result += '<section id="content">';
         result += content;
-        result += '</section>'
+        result += '</section>';
 
         // Page
         result += '<section id="page">';
@@ -37,7 +37,7 @@
             result += ( i++ ? '; ' : '' ) + j + ': ' + page[ j ];
         }
 
-        result += '"></article>'
+        result += '"></article>';
         result += '</section>';
 
         // Style
