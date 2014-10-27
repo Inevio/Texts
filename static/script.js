4f481b67e369add57b0735111852597e55904ef0
Javier Garmón <jgarmon@weezeel.com>
2014-10-27 22:00:41 +0100

    Texts - Fixed handleCharSelection() method

--- static/script.js
+++ static/script.js
@@ -3109,87 +3109,8 @@ var handleCharSelection = function( newChar ){
         charInParagraphEnd += currentRangeEnd.paragraph.lineList[ i ].totalChars;
     }
 
-    // Si está en el mismo nodo
-    if(
-        currentRangeStart.pageId === currentRangeEnd.pageId &&
-        currentRangeStart.paragraphId === currentRangeEnd.paragraphId &&
-        currentRangeStart.lineId === currentRangeEnd.lineId &&
-        currentRangeStart.nodeId === currentRangeEnd.nodeId
-    ){
-
-        currentRangeStart.line.totalChars -= currentRangeStart.node.string.length;
-        currentRangeStart.node.string      = currentRangeStart.node.string.slice( 0, currentRangeStart.nodeChar ) + newChar + currentRangeStart.node.string.slice( currentRangeEnd.nodeChar );
-        currentRangeStart.line.totalChars += currentRangeStart.node.string.length;
-        
-        measureNode( currentRangeStart.paragraph, currentRangeStart.line, currentRangeStart.lineId, currentRangeStart.lineChar, currentRangeStart.node, currentRangeStart.nodeId, currentRangeStart.nodeChar );
-        
-    // Si está en la misma línea pero en distintos nodos
-    }else if(
-        currentRangeStart.pageId === currentRangeEnd.pageId &&
-        currentRangeStart.paragraphId === currentRangeEnd.paragraphId &&
-        currentRangeStart.lineId === currentRangeEnd.lineId
-    ){
-
-        // Nodo inicial
-        currentRangeStart.line.totalChars -= currentRangeStart.node.string.length;
-        currentRangeStart.node.string      = currentRangeStart.node.string.slice( 0, currentRangeStart.nodeChar ) + newChar;
-        currentRangeStart.line.totalChars += currentRangeStart.node.string.length;
-
-        measureNode( currentRangeStart.paragraph, currentRangeStart.line, currentRangeStart.lineId, currentRangeStart.lineChar, currentRangeStart.node, currentRangeStart.nodeId, currentRangeStart.nodeChar );
-
-        // Eliminado de nodos intermedios
-        for( i = currentRangeStart.nodeId + 1; i < currentRangeEnd.nodeId; i++ ){
-            currentRangeStart.line.totalChars -= currentRangeStart.line.nodeList[ i ].string.length;
-        }
-
-        currentRangeStart.line.nodeList = currentRangeStart.line.nodeList.slice( 0, currentRangeStart.nodeId + 1 ).concat( currentRangeEnd.line.nodeList.slice( currentRangeEnd.nodeId ) );
-
-        // Nodo final
-        currentRangeEnd.line.totalChars -= currentRangeEnd.node.string.length;
-        currentRangeEnd.node.string      = currentRangeEnd.node.string.slice( currentRangeEnd.nodeChar );
-        currentRangeEnd.line.totalChars += currentRangeEnd.node.string.length;
-
-        measureNode( currentRangeEnd.paragraph, currentRangeEnd.line, currentRangeEnd.lineId, currentRangeEnd.lineChar, currentRangeEnd.node, currentRangeEnd.nodeId, currentRangeEnd.nodeChar );
-        
-    // Si están en varias líneas
-    }else{
-
-        // Línea inicial
-        // Nodo inicial
-        currentRangeStart.line.totalChars -= currentRangeStart.node.string.length;
-        currentRangeStart.node.string      = currentRangeStart.node.string.slice( 0, currentRangeStart.nodeChar ) + newChar;
-        currentRangeStart.line.totalChars += currentRangeStart.node.string.length;
-
-        measureNode( currentRangeStart.paragraph, currentRangeStart.line, currentRangeStart.lineId, currentRangeStart.lineChar, currentRangeStart.node, currentRangeStart.nodeId, currentRangeStart.nodeChar );
-
-        // Eliminamos los nodos siguientes de la línea
-        for( i = currentRangeStart.nodeId + 1; i < currentRangeStart.line.nodeList.length; i++ ){
-            currentRangeStart.line.totalChars -= currentRangeStart.line.nodeList[ i ].string.length;
-        }
-
-        currentRangeStart.line.nodeList = currentRangeStart.line.nodeList.slice( 0, currentRangeStart.nodeId + 1 );
-
-        // Líneas intermedias
-        removeRangeLines( false, currentRangeStart, currentRangeEnd );
-
-        // Línea final
-        // Eliminamos los primeros nodes de la línea
-        for( i = 0; i < currentRangeEnd.nodeId; i++ ){
-            currentRangeEnd.line.totalChars -= currentRangeEnd.line.nodeList[ i ].string.length;
-        }
-
-        currentRangeEnd.line.totalChars -= currentRangeEnd.node.string.length;
-        currentRangeEnd.node.string      = currentRangeEnd.node.string.slice( currentRangeEnd.nodeChar );
-        currentRangeEnd.line.totalChars += currentRangeEnd.node.string.length;
-        currentRangeEnd.line.nodeList    = currentRangeEnd.line.nodeList.slice( currentRangeEnd.nodeId );
-
-        measureNode( currentRangeEnd.paragraph, currentRangeEnd.line, currentRangeEnd.lineId, currentRangeEnd.lineChar, currentRangeEnd.node, currentRangeEnd.nodeId, 0 );
-
-    }
-
-    setCursor( currentRangeStart.pageId, currentRangeStart.paragraphId, currentRangeStart.lineId, currentRangeStart.lineChar + 1, currentRangeStart.nodeId, currentRangeStart.nodeChar + 1, true );
-    realocateLineInverse( currentParagraph, currentLineId, currentLineCharId );
-    resetBlink();
+    handleBackspaceSelection();
+    handleChar( newChar );
 
     if( !realtime ){
         return;
