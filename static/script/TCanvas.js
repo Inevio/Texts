60cd5da96e9574c3d1aa05a76b07d6365fbaca7e
Javier Garmon <jgarmon@inevio.com>
2015-06-30 10:45:27 +0200

    Testing ES6

--- static/script/TCanvas.js
+++ static/script/TCanvas.js
@@ -1,44 +1,48 @@
 
-var TCanvas = function(){
+class TCanvas{
 
-    this.canvas  = undefined;
-    this.ctx     = undefined;
-    this.waiting = false;
+    constructor(){
 
-};
+        this.canvas  = undefined;
+        this.ctx     = undefined;
+        this.waiting = false;
 
-TCanvas.prototype.requestDraw = function(){
+    };
 
-    if( this.waiting ){
-        return;
-    }
+    requestDraw(){
 
-    this.waiting = true;
+        if( this.waiting ){
+            return;
+        }
 
-    requestAnimationFrame( this.draw.bind( this ) );
+        this.waiting = true;
 
-};
+        requestAnimationFrame( this.draw.bind( this ) );
 
-TCanvas.prototype.updateSize = function(){
+    };
 
-    if( activeHiRes ){
+    updateSize(){
 
-        var oldWidth  = this.canvas.width();
-        var oldHeight = this.canvas.height();
+        if( activeHiRes ){
 
-        this.canvas[ 0 ].width  = oldWidth * pixelRatio;
-        this.canvas[ 0 ].height = oldHeight * pixelRatio;
+            var oldWidth  = this.canvas.width();
+            var oldHeight = this.canvas.height();
 
-        this.canvas[ 0 ].style.width  = oldWidth + 'px';
-        this.canvas[ 0 ].style.height = oldHeight + 'px';
+            this.canvas[ 0 ].width  = oldWidth * pixelRatio;
+            this.canvas[ 0 ].height = oldHeight * pixelRatio;
 
-        this.ctx.scale( pixelRatio, pixelRatio );
+            this.canvas[ 0 ].style.width  = oldWidth + 'px';
+            this.canvas[ 0 ].style.height = oldHeight + 'px';
 
-    }else{
+            this.ctx.scale( pixelRatio, pixelRatio );
 
-        this.canvas[ 0 ].width  = this.canvas.width();
-        this.canvas[ 0 ].height = this.canvas.height();
+        }else{
 
-    }
+            this.canvas[ 0 ].width  = this.canvas.width();
+            this.canvas[ 0 ].height = this.canvas.height();
+
+        }
+
+    };
 
 };
