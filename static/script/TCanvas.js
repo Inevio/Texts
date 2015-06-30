2c55dfc143661cd183c929399172f8e241a9b21e
Javier Garmon <jgarmon@inevio.com>
2015-06-30 11:00:27 +0200

    Revert "Testing ES6"
    This reverts commit 60cd5da96e9574c3d1aa05a76b07d6365fbaca7e.

--- static/script/TCanvas.js
+++ static/script/TCanvas.js
@@ -1,48 +1,44 @@
 
-class TCanvas{
+var TCanvas = function(){
 
-    constructor(){
+    this.canvas  = undefined;
+    this.ctx     = undefined;
+    this.waiting = false;
 
-        this.canvas  = undefined;
-        this.ctx     = undefined;
-        this.waiting = false;
+};
 
-    };
+TCanvas.prototype.requestDraw = function(){
 
-    requestDraw(){
+    if( this.waiting ){
+        return;
+    }
 
-        if( this.waiting ){
-            return;
-        }
+    this.waiting = true;
 
-        this.waiting = true;
+    requestAnimationFrame( this.draw.bind( this ) );
 
-        requestAnimationFrame( this.draw.bind( this ) );
+};
 
-    };
+TCanvas.prototype.updateSize = function(){
 
-    updateSize(){
+    if( activeHiRes ){
 
-        if( activeHiRes ){
+        var oldWidth  = this.canvas.width();
+        var oldHeight = this.canvas.height();
 
-            var oldWidth  = this.canvas.width();
-            var oldHeight = this.canvas.height();
+        this.canvas[ 0 ].width  = oldWidth * pixelRatio;
+        this.canvas[ 0 ].height = oldHeight * pixelRatio;
 
-            this.canvas[ 0 ].width  = oldWidth * pixelRatio;
-            this.canvas[ 0 ].height = oldHeight * pixelRatio;
+        this.canvas[ 0 ].style.width  = oldWidth + 'px';
+        this.canvas[ 0 ].style.height = oldHeight + 'px';
 
-            this.canvas[ 0 ].style.width  = oldWidth + 'px';
-            this.canvas[ 0 ].style.height = oldHeight + 'px';
+        this.ctx.scale( pixelRatio, pixelRatio );
 
-            this.ctx.scale( pixelRatio, pixelRatio );
+    }else{
 
-        }else{
+        this.canvas[ 0 ].width  = this.canvas.width();
+        this.canvas[ 0 ].height = this.canvas.height();
 
-            this.canvas[ 0 ].width  = this.canvas.width();
-            this.canvas[ 0 ].height = this.canvas.height();
-
-        }
-
-    };
+    }
 
 };
